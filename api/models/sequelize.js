1510d5f7ada6a1346737e076e4b840b435a75509
mohamed_twfik <mohamedtwfik910@gmail.com>
2023-04-08 11:36:56 +0200

    replace nodemon by node

--- api/models/sequelize.js
+++ api/models/sequelize.js
@@ -1,13 +1,6 @@
 const { Sequelize } = require("sequelize")
 const config = require("config")
-// const dbConfig = config.get("dbConfig")
-const dbConfig = {
-  host:"localhost",
-  database: "smart_farm",
-  user:"root",
-  password:"",
-  dialect:"mysql"
-}
+const dbConfig = config.get("dbConfig")
 
 module.exports = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
   host: dbConfig.host,
