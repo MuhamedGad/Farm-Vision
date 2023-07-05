e419c7328d9fec96ada5a0d1742b5535329cf483
mohamed_twfik <mohamedtwfik910@gmail.com>
2023-07-05 03:14:04 +0300

    dbconfig

--- api/models/sequelize.js
+++ api/models/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize")
 const config = require("config")
-// const dbConfig = config.get("dbConfig")
+const dbConfig = config.get("dbConfig")
 // const dbConfig= {
 //   host    : "localhost",
 //   user    : "root",
@@ -9,14 +9,14 @@ const config = require("config")
 //   port    : "",
 //   dialect : "mysql"
 // }
-const dbConfig= {
-  host    : "localhost",
-  user    : "postgres",
-  password: "mohamed910",
-  database: "smart_farm",
-  port    : 5432,
-  dialect : "postgres"
-}
+// const dbConfig= {
+//   host    : "localhost",
+//   user    : "postgres",
+//   password: "mohamed910",
+//   database: "smart_farm",
+//   port    : 5432,
+//   dialect : "postgres"
+// }
 module.exports = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
   host: dbConfig.host,
   port: dbConfig.port,
