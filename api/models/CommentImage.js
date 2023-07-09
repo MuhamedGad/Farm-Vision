bfd870f244b15505654f7ea01c224ea204ef49fa
mohamed_twfik <mohamedtwfik910@gmail.com>
2023-07-09 19:55:18 +0300

    comment image

--- api/models/CommentImage.js
+++ api/models/CommentImage.js
@@ -3,9 +3,9 @@ const sequelize = require("./sequelize")
 const Comment = require("./Comment")
 
 const CommentImage = sequelize.define('CommentImage', {
-    // image: {
-    //     type: DataTypes.BLOB("long"),
-    // },
+    image: {
+        type: DataTypes.BLOB("long"),
+    },
 })
 
 Comment.hasMany(CommentImage, {
